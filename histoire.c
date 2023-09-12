707a25de87241cf4e7dec516930603b31ea98ba6
agathemczk <agathe.maciaszczyk@icloud.com>
2023-09-12 12:28:18 +0200

    commit

--- histoire.c
+++ histoire.c
@@ -1,3 +1,11 @@
-//
-// Created by agath on 12/09/2023.
-//
+#include <stdlib.h>
+
+int startadventure ; {
+printf("Vous êtes un chasseur de prime réputé pour votre détermination. Etant enfant, votre petite soeur s'est faite enlevée par le montre $. Depuis, votre seul but est de venger votre soeur et protéger le village.\n");
+printf("Choisissez un pseudo : \n") ;
+scanf("%c",&);
+printf("Maintenant, choisissez une classe. Si vous préférez combattre avec vos poings, prenez la classe rapprochée 'R'. Si vous adorez utiliser un arc, choisissez la classe distance 'D': \n") ;
+scanf("%c",&);
+printf("Vous voilà donc prêt à combattre !\n") ;
+
+}
