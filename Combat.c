ae6c1f5fbd8862af4a22f1f9a82e135053901c6b
Charles-Antoine <charto.bta@gmail.com>
2023-09-12 11:36:45 +0200

    v0.1
    déclaration des variables

---
+++ Combat.c
@@ -0,0 +1,11 @@
+char dmg(struct classe player, struct classe mob){
+    
+}
+
+char dodge(struct classe player, struct classe mob){
+
+}
+
+char state(struct classe player, struct classe mob){
+
+}
